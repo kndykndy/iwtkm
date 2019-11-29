1de14a18d9c58b36f048bf5f98cce1a43e9abe57
kndykndy <eakrapivin@gmail.com>
2019-11-29 09:19:15 +0100

    Leetcode #11

---
+++ leetcode/src/main/java/com/kndykndy/leetcode/p11_ContainerWithMostWater.java
@@ -0,0 +1,39 @@
+package com.kndykndy.leetcode;
+
+public class p11_ContainerWithMostWater {
+
+    interface Solution {
+
+        int maxArea(int[] height);
+    }
+
+    /**
+     * Time O(n), space O(1).
+     */
+    public static class Solution1 implements Solution {
+
+        @Override
+        public int maxArea(int[] height) {
+            int maxSquare = 0;
+            int i = 0, j = height.length - 1;
+            while (i < j) {
+                maxSquare = Math.max(maxSquare, (j - i) * Math.min(height[i], height[j]));
+                if (height[i] < height[j]) {
+                    i++;
+                } else if (height[j] < height[i]) {
+                    j--;
+                } else {
+                    if (height[i + 1] < height[j - 1]) {
+                        j--;
+                    } else if (height[j - 1] < height[i + 1]) {
+                        i++;
+                    } else {
+                        i++;
+                        j--;
+                    }
+                }
+            }
+            return maxSquare;
+        }
+    }
+}
