c3a7f5234740d7a9a217a67da9cdd04e4140ef0d
kndykndy <eakrapivin@gmail.com>
2019-12-24 23:32:24 +0100

    Leetcode #45

---
+++ leetcode/src/test/java/com/kndykndy/leetcode/p45_JumpGameIITest.java
@@ -0,0 +1,31 @@
+package com.kndykndy.leetcode;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.kndykndy.leetcode.p45_JumpGameII.BruteForceSolution;
+import com.kndykndy.leetcode.p45_JumpGameII.DpMemoizationSolution;
+import com.kndykndy.leetcode.p45_JumpGameII.Solution;
+import java.util.Arrays;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class p45_JumpGameIITest {
+
+    private List<Solution> solutions;
+
+    @BeforeEach
+    void setUp() {
+        solutions = Arrays.asList(new BruteForceSolution(), new DpMemoizationSolution());
+    }
+
+    @Test
+    void test1() {
+        for (Solution solution : solutions) {
+            assertEquals(2, solution.jump(new int[]{2, 3, 1, 1, 4}));
+            assertEquals(2, solution.jump(new int[]{1, 2, 3}));
+            assertEquals(2, solution.jump(new int[]{2, 3, 0, 1, 4}));
+            assertEquals(3, solution.jump(new int[]{1, 2, 1, 1, 1}));
+        }
+    }
+}
