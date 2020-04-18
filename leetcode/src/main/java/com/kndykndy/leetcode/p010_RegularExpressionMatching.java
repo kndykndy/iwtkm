1c892edc1b1519fd3d4cfcaefe5f383a26e062a8
kndykndy <eakrapivin@gmail.com>
2020-04-18 12:56:26 +0200

    Leetcode #10 draft

---
+++ leetcode/src/main/java/com/kndykndy/leetcode/p010_RegularExpressionMatching.java
@@ -0,0 +1,16 @@
+package com.kndykndy.leetcode;
+
+public class p010_RegularExpressionMatching {
+
+    interface Solution {
+        boolean isMatch(String s, String p);
+    }
+
+    public static class Solution1 implements Solution {
+
+        @Override
+        public boolean isMatch(String s, String p) {
+            return false; //todo
+        }
+    }
+}
---
+++ leetcode/src/test/java/com/kndykndy/leetcode/p010_RegularExpressionMatchingTest.java
@@ -0,0 +1,25 @@
+package com.kndykndy.leetcode;
+
+import com.kndykndy.leetcode.p010_RegularExpressionMatching.Solution;
+import com.kndykndy.leetcode.p010_RegularExpressionMatching.Solution1;
+import java.util.Collections;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class p010_RegularExpressionMatchingTest {
+
+    private List<Solution> solutions;
+
+    @BeforeEach
+    void setUp() {
+        solutions = Collections.singletonList(new Solution1());
+    }
+
+    @Test
+    void test1() {
+        for (Solution solution : solutions) {
+            // todo
+        }
+    }
+}
